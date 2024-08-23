c3a53eb77532c43f6e1349453c3ab194c589a4b4
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-08-23 11:34:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+let output = document.getElementById("output");
+let button = document.getElementById("btn"); 
+let text = document.getElementById("text");
+let delay = document.getElementById("delay");
+text.value = "";
+delay.value="";
+output.innerText="";
+
+button.addEventListener("click",displayMessage)
+
+async function displayMessage(){
+	await new Promise((resolve)=>setTimeout(resolve, delay.value));
+	output.innerText=text.value;
+}
